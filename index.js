a6cbc82fe34efd65ffead9eb47d08e854e0230c5
atishe <you@example.com>
2024-07-18 13:10:53 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+require("dotenv").config()
+
+const app = express()
+app.use(express.json)
+app.use(express.static("dist"))
+app.use(cors({
+    origin: process.env.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : "live-server",
+    credentials: true
+}))
+app.use("*", (req, res,) => {
+    res.status(404).json({ message: "resorce not found" })
+})
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({ message: `server Error${err.message}` })
+})
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONECTED")
+    app.listen(process.env.PORT, console.log(`SERVER RUNNING`))
+})
