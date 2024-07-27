04e956c11bd06316f1b7b7ee97f68c10afcc78ef
atishe <you@example.com>
2024-07-27 13:13:01 +0530

    user email and mobile otp fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,9 @@ const sendEmail = require("../utils/email")
 
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In.Plese LOgin Again" })
+    }
     const otp = Math.floor(10000 + Math.random() * 900000)
     await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })
     const isSend = await sendEmail({
@@ -14,4 +17,29 @@ exports.verifyUserEmail = asyncHandler(async (req, res) => {
 
 
     res.json({ message: "Verification Send Success" })
+})
+
+exports.verifyEmailOTP = asyncHandler(async (req, res) => {
+    const { otp } = req.body
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In.Plese LOgin Again" })
+    }
+    if (otp !== result.emailcode) {
+        return res.status(400).json({ message: "INvalid OTP" })
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { emailverified: true })
+    res.json({ message: "Email Verify Success" })
+})
+exports.verifyMobilOTP = asyncHandler(async (req, res) => {
+    const { otp } = req.body
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In.Plese LOgin Again" })
+    }
+    if (otp !== result.mobailcode) {
+        return res.status(400).json({ message: "INvalid OTP" })
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { emailverified: true })
+    res.json({ message: "Email Verify Success" })
 })
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,7 @@ const router = require("express").Router()
 router
 
     .post("/verify-email", userProtected, userController.verifyUserEmail)
+    .post("/verify-user-email-otp", userProtected, userController.verifyEmailOTP)
+    .post("/verify-user-mobile-otp", userProtected, userController.verifyMobilOTP)
 
 module.exports = router
